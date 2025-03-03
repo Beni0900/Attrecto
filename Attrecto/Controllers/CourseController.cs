f89368984be31fd0134cbe80d769ac592e8ff7a8
Darabos Benjamin <darabosbeni@gmail.com>
2025-03-03 13:18:32 +0100

    Course Controller uploaded

---
+++ Attrecto/Controllers/CourseController.cs
@@ -0,0 +1,87 @@
+﻿using Attrecto.Data;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Attrecto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        public static List<Course> Courses = new List<Course>();
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Course> Get()
+        {
+            return Courses;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Course> Get(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    return Ok(course);
+                }
+            }
+
+            return BadRequest();
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Course data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Courses.Add(data);
+
+            return NoContent();
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Course data)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    course.Name = data.Name;
+                    course.Description = data.Description;
+
+                    return NoContent();
+                }
+            }
+
+            return NotFound();
+        }
+
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    Courses.Remove(course);
+
+                    return NoContent();
+                }
+            }
+
+            return NotFound();
+        }
+    }
+}
